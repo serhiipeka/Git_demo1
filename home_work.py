c892d04938371147f4c7aa0bfe41d65066f54d00
JennyJones01 <epidkevych@gmail.com>
2023-04-03 12:52:57 -0500

    Home work with classes

---
+++ home_work.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print("I can run")
+
+dog1 = Dog("Jack", 5)
+dog2 = Dog("Snooppy", 7)
+print(dog2.name)
+dog2.run()
+
